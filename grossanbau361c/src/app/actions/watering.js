d04983f81eb04d5bb7e2ee4a1c6e4c98ff540efd
MaxSchild <m.schild23@icloud.com>
2024-07-19 21:52:56 +0000

    Add first page

---
+++ grossanbau361c/src/app/actions/watering.js
@@ -0,0 +1,9 @@
+"use server";
+
+export async function getWateringStatus() {
+
+}
+
+export async function waterPlants() {
+    console.log("Pflanzen gegossen");
+}
--- grossanbau361c/src/app/page.js
+++ grossanbau361c/src/app/page.js
@@ -1,8 +1,33 @@
+"use client";
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+
+import IconButton from '@mui/material/IconButton';
+import { waterPlants } from '@/app/actions/watering.js'
 export default function Home() {
   return (
-    <Typography>
-      Hello World!
-    </Typography>
+    <>
+
+      <Stack direction="column">
+        <Typography variant="h1">
+          Gegossen?
+        </Typography>
+
+        <IconButton aria-label="waterPlants" onClick={async () => {
+
+          waterPlants();
+        }} color="success">
+          <WaterDropIcon />
+        </IconButton>
+
+
+        <Typography >
+          Done
+        </Typography>
+      </Stack>
+
+    </>
   );
 }
