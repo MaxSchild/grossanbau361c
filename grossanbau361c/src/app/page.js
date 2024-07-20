52f0214c0ccde849849e77a46b41f5b5113088ee
MaxSchild <m.schild23@icloud.com>
2024-07-20 00:49:06 +0000

    Add disabling button after click or when already watered

--- grossanbau361c/src/app/page.js
+++ grossanbau361c/src/app/page.js
@@ -15,6 +15,7 @@ export default function Home({ data }) {
 
   const [isLoaded, setIsLoaded] = useState(false);
   const [wateringStatus, setWateringStatus] = useState();
+  const [isDisabled, setIsDisabled] = useState(false);
 
   useEffect(() => {
     if (!isLoaded) {
@@ -43,14 +44,22 @@ export default function Home({ data }) {
               <CircularProgress />
             </Box>
             :
-            <><IconButton aria-label="waterPlants" onClick={async () => {
-
+            <><IconButton aria-label="waterPlants" disabled={isDisabled || wateringStatus.wasWateredToday} onClick={async () => {
+              setIsDisabled(true);
               waterPlants();
+              getWateringStatus().then(
+                (res) => {
+                  console.log("Loaded Values into client:");
+                  console.log(res);
+                  setWateringStatus(res)
+                  setIsDisabled(false);
+                });
+
             }} color={wateringStatus.wasWateredToday ? "success" : "error"}>
               <WaterDropIcon />
             </IconButton>
               <Typography >
-                Done
+                {wateringStatus.wasWateredToday ? "Fertig" : "Bitte noch gießen!"}
               </Typography>
 
             </>
